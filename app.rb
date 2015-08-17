b6e7d15e0c05b73c557e8f1d8c17a7c2c99e48c4
yubata <ft.zr.oath.bsk716@gmail.com>
2015-08-17 06:03:02 +0000

    08 layout

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 ## require 'sinatra/reloader
 
-get '/:name' do |n|
-  # "hello #{n}"
-  @name = n
+get '/' do
   @title = "main index"
+  @content = "main content"
   erb :index
+end
+
+get'/about' do
+  @title = "about this page"
+  @content = "this page is ..."
+  @email = "taguchi@email.com"
+  erb :about
 end

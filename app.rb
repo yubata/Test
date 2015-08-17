a4df4303b1fb975d79f8876118a2bbf83663beb6
yubata <ft.zr.oath.bsk716@gmail.com>
2015-08-17 05:23:27 +0000

    04 URLからのパラメータ入力2

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 ## require 'sinatra/reloader
 
-
+=begin
 get '/hello/:name' do
   "hello #{params[:name]}"
 end
 
 
+get '/hello/:name' do |n|
+  "hello #{n}"
+end
+=end
+
+
+get '/hello/:fname/?:lname' do |f,l|
+  "hello #{f} #{l}"
+end

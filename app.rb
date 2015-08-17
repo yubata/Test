50901174496b50f737cd2cfc0f55e0b82e314ee9
yubata <ft.zr.oath.bsk716@gmail.com>
2015-08-17 07:31:17 +0000

    13 comment

--- app.rb
+++ app.rb
@@ -1,40 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 
-before '/admin/*' do
-  @msg = "admin area!"
-end
+ActiveRecord::Base.establish_connection(
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
+  )
 
-before do
-  @author = "taguchi"
-end
-
-after do
-  logger.info "page displayed successfully"
-end
-
-helpers do
-  def strong(s)
-    "<strong>#{s}</strong>"
-  end
+class Comment < ActiveRecord::Base
 end
 
 get '/' do
-  @title = "main"
-  @content = "main content by " + @author
+  @comments = Comment.order("id desc").all
   erb :index
-end
-
-get '/about' do
-  @title = "about this page"
-  @content = "this page is ... by " + strong(@author)
-  @email = "taguchi@email.com"
-  erb :about
-end
-
-  get'/admin/' do
-  @title = "about this page"
-  @content = "this page is ... by " + strong(@msg)
-  @email = "taguchi@email.com"
-  erb :about
 end

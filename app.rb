8f4fce0b47eec2967eca02e52376bd33ba7e7202
yubata <ft.zr.oath.bsk716@gmail.com>
2015-08-17 05:39:57 +0000

    05 ERB templete

--- app.rb
+++ app.rb
@@ -1,12 +1,6 @@
 require 'sinatra'
 ## require 'sinatra/reloader
 
-=begin
-get '/from/*/to/*' do |f,t|
-   "from #{f} to #{t}"
-end
-=end
-
-get %r{/users/([0-9]*)} do |i|
-  "user id = #{i}"
+get '/' do 
+  erb :index
 end

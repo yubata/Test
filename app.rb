c854df732ce29f91697458fb0cdadc41ce1188ba
yubata <ft.zr.oath.bsk716@gmail.com>
2015-08-17 06:21:39 +0000

    10 helper

--- app.rb
+++ app.rb
@@ -13,6 +13,12 @@ after do
   logger.info "page displayed successfully"
 end
 
+helpers do
+  def strong(s)
+    "<strong>#{s}</strong>"
+  end
+end
+
 get '/' do
   @title = "main index"
   @content = "main content by " + @author
@@ -21,7 +27,7 @@ end
 
 get'/about' do
   @title = "about this page"
-  @content = "this page is ... by " + @author
+  @content = "this page is ... by " + strong(@author)
   @email = "taguchi@email.com"
   erb :about
 end

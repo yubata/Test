08e23d8f78ddad071b08422cff13ffe2035bec73
yubata <ft.zr.oath.bsk716@gmail.com>
2015-08-17 05:31:46 +0000

    04 URLからのパラメータ（２）

--- app.rb
+++ app.rb
@@ -2,17 +2,11 @@ require 'sinatra'
 ## require 'sinatra/reloader
 
 =begin
-get '/hello/:name' do
-  "hello #{params[:name]}"
-end
-
-
-get '/hello/:name' do |n|
-  "hello #{n}"
+get '/from/*/to/*' do |f,t|
+   "from #{f} to #{t}"
 end
 =end
 
-
-get '/hello/:fname/?:lname' do |f,l|
-  "hello #{f} #{l}"
+get %r{/users/([0-9]*)} do |i|
+  "user id = #{i}"
 end

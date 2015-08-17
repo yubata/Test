8e814fca7e87aeb1340b2c5206a1d2e894508951
yubata <ft.zr.oath.bsk716@gmail.com>
2015-08-17 06:14:37 +0000

    09 before_after

--- app.rb
+++ app.rb
@@ -1,15 +1,27 @@
 require 'sinatra'
 ## require 'sinatra/reloader
 
+before '/admin/*' do
+  @msg = "admin area!"
+end
+
+before do
+  @author = "taguchi"
+end
+
+after do
+  logger.info "page displayed successfully"
+end
+
 get '/' do
   @title = "main index"
-  @content = "main content"
+  @content = "main content by " + @author
   erb :index
 end
 
 get'/about' do
   @title = "about this page"
-  @content = "this page is ..."
+  @content = "this page is ... by " + @author
   @email = "taguchi@email.com"
   erb :about
 end
